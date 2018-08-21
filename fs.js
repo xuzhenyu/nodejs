cdda54f8dfb163817014be1ae7b84d3bca25a9cb
xuzhenyu <xiiaoyu@vip.qq.com>
2018-08-21 16:18:37 +0800

    http协议

--- fs.js
+++ fs.js
@@ -1,17 +1,27 @@
 const http = require("http");
 const fs = require("fs");
 http.createServer(function(req,res){
-    res.write('8888');
-    const filename = '../nodejs'+req.url;
-    fs.readFile(filename,(err,data)=>{
-        if(err){
-            res.write('404');
-        }else{
-            res.write(data);
-        }
-        res.end();
-    })
-}).listen(9091);
+    // res.write('8888');
+    // const filename = '../nodejs'+req.url;
+    // fs.readFile(filename,(err,data)=>{
+    //     if(err){
+    //         res.write('404');
+    //     }else{
+    //         res.write(data);
+    //     }
+    //     res.end();
+    // })
+
+    if(req.url === '/'){
+        const html = fs.readFileSync('form.html','utf8')
+        res.writeHead(200,{
+            'Content-Type':'text/html',
+            'Set-Cookie':['id=123;max-age=2','abc=456']
+        })
+        res.end(html);
+    }
+
+}).listen(9090);
 
 // fs.readFile('111.txt',(err,data)=>{
 //     if(err){
